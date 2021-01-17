e28d808c0d00aa873fc8b7e872b50afd8f8a3087
shodayuki <yshoda1227@gmail.com>
2021-01-17 11:50:40 +0900

    ログイン成功したときの統合テストを実装

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,10 +2,15 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-  test "login with invalid information" do
+  def setup
+    @user = users(:michael)
+  end
+
+  # メールアドレスが正しく、パスワードが間違っているときのテスト
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: @user.email, password: "invalid" } }
     assert_not flash.empty?
     get root_path
     assert flash.empty?
